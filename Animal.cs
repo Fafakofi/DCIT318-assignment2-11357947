016f50f25ecc146d6960167a9bd0ac68f3180acc
fafakofi <sfkbenson@st.ug.edu.gh>
2025-08-08 18:10:39 +0000

    Inheritance and Method Overriding

---
+++ Animal.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Inheritance and method overloading
+        Animal myAnimal = new Animal();
+        Animal myDog = new Dog();
+        Animal myCat = new Cat();
+
+        myAnimal.MakeSound();
+        myDog.MakeSound();
+        myCat.MakeSound();
+    }
+}
